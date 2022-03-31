8f9be3a18ac36f15d44240b0cbcda0cd2a18f74a
kmgtk <56664447+kmgtk@users.noreply.github.com>
2022-03-31 23:40:24 +0900

    도메인 설계 Commit

---
+++ src/main/java/com/example/demo/domain/item/Item.java
@@ -0,0 +1,34 @@
+package com.example.demo.domain.item;
+
+import com.example.demo.domain.Category;
+import com.example.demo.domain.OrderItem;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+public abstract class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
+
+}
