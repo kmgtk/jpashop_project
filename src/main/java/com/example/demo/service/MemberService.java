a9c189e008f0fe050edfb31219b52665be9b62d6
kmgtk <56664447+kmgtk@users.noreply.github.com>
2022-04-02 00:15:37 +0900

    회원 서비스 commit

---
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,49 @@
+package com.example.demo.service;
+
+import com.example.demo.Repository.MemberRepository;
+import com.example.demo.domain.Member;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    @Autowired
+   private final MemberRepository memberRepository;
+
+    //회원가입
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+
+        List<Member> findMembers =  memberRepository.findByName(member.getName());
+
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+        //Excpetion
+    }
+
+    //회원 전체 조회
+    @Transactional(readOnly = true)
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+    @Transactional(readOnly = true)
+    public Member findOne(Long id){
+        return memberRepository.findOne(id);
+    }
+}
