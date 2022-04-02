358ede034e9335f533ad02c8daf23270e4a04c64
kmgtk <56664447+kmgtk@users.noreply.github.com>
2022-04-03 01:00:25 +0900

    commit

--- src/test/java/com/example/demo/service/MemberServiceTest.java
+++ src/test/java/com/example/demo/service/MemberServiceTest.java
@@ -6,10 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-<<<<<<< HEAD
-=======
-import org.springframework.test.annotation.Rollback;
->>>>>>> origin/master
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
